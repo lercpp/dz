0b4eb6c30cba95272109187e0809d0c207eb5fcc
lercpp <flashiap08@gmail.com>
2025-06-04 16:40:00 +0300

    №1

--- profil.py
+++ profil.py
@@ -1,6 +1,47 @@
 
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+
+def run():
+    num1 = num_1_enty.get()
+    combo1=combobox1.get()
+    combo2=combobox1.get()
+
+    if combo1==combobox2:
+         messagebox.showerror(title="валюта", message="Вы ввели одинаковый тип валюты")
+
+    else:
+         
+        if combo1=="RUB" and combo2=="EUR":
+            combo1=int(combo1)
+            a=combo1/89
+            num_2_enty.config(text=f"{a}")
+
+        if combo1=="RUB" and combo2=="GBP":
+            combo1=int(combo1)
+            d=combo1/106
+            num_2_enty.config(text=f"{d}")
+        
+        if combo1=="EUR" and combo2=="RUB":
+            combo1=int(combo1)
+            h=combo1*89
+            num_2_enty.config(text=f"{h}")
+
+        if combo1=="EUR" and combo2=="GBP":
+            combo1=int(combo1)
+            u=combo1/1,19
+            num_2_enty.config(text=f"{u}")
+
+        if combo1=="GBP" and combo2=="RUB":
+            combo1=int(combo1)
+            p=combo1*106
+            num_2_enty.config(text=f"{p}")
+
+        if combo1=="GBP" and combo2=="EUR":
+            combo1=int(combo1)
+            i=combo1*1,19
+            num_2_enty.config(text=f"{i}")
 
 
 root = Tk()
@@ -32,6 +73,8 @@ num_2_enty.grid(row=2, column=1)
 
 combobox2 = ttk.Combobox(content_1, values=languages)  
 combobox2.grid(row=2, column=2)  
+
+but=Button(content_1,text="Go",command=run)
 
 content_1.pack()
 
